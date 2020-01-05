2b6b0b0adc966b87adf0b6593b25af122e59cd78
sebashack <sebastian0092@gmail.com>
2020-01-05 18:29:23 -0500

    Implement cost function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
-use  ml_tool_box::regression::linear::add;
+use ml_tool_box::regression::linear::{compute_cost};
+
+use nalgebra::{DMatrix, DVector};
 
 fn main() {
-    println!("The result is {}", add(1, 1));
+    let x = DMatrix::from_iterator(3, 3, vec![1.0, 4.0, 7.0,
+                                              2.0, 5.0, 8.0,
+                                              3.0, 6.0, 9.0]);
+    let y = DVector::from_iterator(3, vec![4.0, 6.0, 8.0]);
+    let theta = DVector::from_iterator(3, vec![4.0, 6.0, 8.0]);
+
+    println!("Result {}", compute_cost(&x, &y, &theta));
 }
--- src/regression/linear.rs
+++ src/regression/linear.rs
@@ -1,3 +1,13 @@
-pub fn add(x: i64, y: i64) -> i64 {
-    x + y
+extern crate nalgebra;
+
+use nalgebra::{DMatrix, DVector};
+
+pub fn compute_cost(x: &DMatrix<f64>, y: &DVector<f64>, theta: &DVector<f64>) -> f64 {
+    let m = y.len() as f64;
+    let a = (x * theta) - y;
+    let atr = a.transpose();
+    let atr_by_a = atr * a;
+    let r = atr_by_a.index((0, 0));
+
+    *r / (2.0 * m)
 }

9879db32be6af09a68c0ec789fa2d4ef3fcc8573
sebashack <sebastian0092@gmail.com>
2020-01-06 00:54:03 -0500

    Implement gradient descent and feature normalize

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
-use ml_tool_box::regression::linear::{compute_cost};
+use ml_tool_box::regression::linear::{compute_cost, gradient_descent, featureNormalize};
 
 use nalgebra::{DMatrix, DVector};
 
 fn main() {
-    let x = DMatrix::from_iterator(3, 3, vec![1.0, 4.0, 7.0,
-                                              2.0, 5.0, 8.0,
-                                              3.0, 6.0, 9.0]);
-    let y = DVector::from_iterator(3, vec![4.0, 6.0, 8.0]);
-    let theta = DVector::from_iterator(3, vec![4.0, 6.0, 8.0]);
+    let x = DMatrix::from_iterator(3, 3, vec![1000.0, 400.0, 70.0,
+                                              2000.0, 500.0, 80.0,
+                                              3000.0, 600.0, 90.0]);
+    let y = DVector::from_iterator(3, vec![4000.0, 600.0, 80.0]);
+    let theta = DVector::from_iterator(3, vec![0.0, 0.0, 0.0]);
 
-    println!("Result {}", compute_cost(&x, &y, &theta));
+    let normalized_x = featureNormalize(&x);
+
+    println!("Result {}", normalized_x);
 }
--- src/regression/linear.rs
+++ src/regression/linear.rs
@@ -1,6 +1,6 @@
 extern crate nalgebra;
 
-use nalgebra::{DMatrix, DVector};
+use nalgebra::{DMatrix, DVector, Matrix, Dynamic, U1, SliceStorage};
 
 pub fn compute_cost(x: &DMatrix<f64>, y: &DVector<f64>, theta: &DVector<f64>) -> f64 {
     let m = y.len() as f64;
@@ -39,4 +39,45 @@ pub fn gradient_descent(x: &DMatrix<f64>,
     }
 
     new_theta
+}
+
+pub fn featureNormalize(x: &DMatrix<f64>) -> DMatrix<f64> {
+    let num_rows = x.nrows();
+    let num_cols = x.ncols();
+
+    let mut normalized_x = DMatrix::from_element(num_rows, num_cols, 0f64);
+
+    for i in 0..num_cols {
+        let vals = x.column(i);
+        let mean = computeMxSliceMean(num_cols, &vals);
+        let std = computeMxSliceStd(num_cols, mean, &vals);
+
+        let new_col = DVector::from_fn(num_cols, |j, _| {
+            (x.index((j, i)) - mean) / std
+        });
+
+        normalized_x.set_column(i, &new_col);
+    }
+
+    normalized_x
+}
+
+fn computeMxSliceMean(n: usize, x: &Matrix<f64, Dynamic, U1, SliceStorage<'_, f64, Dynamic, U1, U1, Dynamic>>) -> f64 {
+    let mut sum: f64 = 0.0;
+
+    for v in x.iter() {
+        sum = sum + v;
+    }
+
+    sum / (n as f64)
+}
+
+fn computeMxSliceStd(n: usize, mean: f64, x: &Matrix<f64, Dynamic, U1, SliceStorage<'_, f64, Dynamic, U1, U1, Dynamic>>) -> f64 {
+    let mut sum_of_squares: f64 = 0.0;
+
+    for v in x.iter() {
+        sum_of_squares = sum_of_squares + (v - mean).powf(2.0);
+    }
+
+    (sum_of_squares / ((n - 1) as f64)).sqrt()
 }
